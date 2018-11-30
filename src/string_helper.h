b1dd7787830b6a3afa09ab82fc7d2d766c04e9e7
ruleless <ruleless@126.com>
2018-11-30 17:51:50 -0500

    add some string helper functions

--- src/string_helper.h
+++ src/string_helper.h
@@ -2,12 +2,90 @@
 #define __STRING_HELPER_H__
 
 #include "exception.h"
+#include <algorithm>
 #include <sstream>
 
 namespace util {
 
+template <typename T>
+inline std::basic_string<T>& ltrim(std::basic_string<T> &s)
+{
+    s.erase(s.begin(), std::find_if(s.begin(), s.end(),
+                                    std::not1(std::ptr_fun<int, int>(std::isspace))));
+    return s;
+}
+
+template <typename T>
+inline std::basic_string<T>& rtrim(std::basic_string<T> &s)
+{
+    s.erase(std::find_if(s.rbegin(), s.rend(),
+                         std::not1(std::ptr_fun<int, int>(std::isspace))).base(), s.end());
+    return s;
+}
+
+template <typename T>
+inline std::basic_string<T> trim(std::basic_string<T> s)
+{
+    return ltrim(rtrim(s));
+}
+
+template <typename T>
+inline size_t replace(std::basic_string<T> &str, const std::basic_string<T> &pattern, const std::basic_string<T> &newpat)
+{
+    size_t count = 0;
+    const size_t nsize = newpat.size();
+    const size_t psize = pattern.size();
+
+    for(size_t pos = str.find(pattern, 0);
+        pos != std::basic_string<T>::npos;
+        pos = str.find(pattern, pos+nsize))
+    {
+        str.replace(pos, psize, newpat);
+        count++;
+    }
+
+    return count;
+}
+
+template <typename T>
+inline std::basic_string<T>& uppercase(std::basic_string<T> &str)
+{
+    std::transform(str.begin(), str.end(), str.begin(), ::toupper);
+    return str;
+}
+
+template <typename T>
+inline std::basic_string<T>& lowercase(std::basic_string<T> &str)
+{
+    std::transform(str.begin(), str.end(), str.begin(), ::tolower);
+    return str;
+}
+
+template<typename T>
+inline size_t split(const std::basic_string<T> &s, T c, std::vector<std::basic_string<T>> &v)
+{
+    if(s.size() == 0) {
+        return 0;
+    }
+
+    typename std::basic_string<T>::size_type i = 0;
+    typename std::basic_string<T>::size_type j = s.find(c);
+
+    while(j != std::basic_string<T>::npos) {
+        v.push_back(s.substr(i, j - i));
+        i = ++j; j = s.find(c, j);
+    }
+
+    if(j == std::basic_string<T>::npos) {
+        v.push_back(s.substr(i, s.length() - i));
+    }
+
+    return v.size();
+}
+
+
 template <typename To, typename From>
-To to(From source)
+inline To to(From source)
 {
     std::stringstream interpreter;
     To target;
--- test/string_helper_test.cpp
+++ test/string_helper_test.cpp
@@ -1,24 +1,48 @@
 #include "src/string_helper.h"
 #include <gtest/gtest.h>
 
+using namespace util;
+
 TEST(string_helper, test_to)
 {
     std::string str;
-    str = util::to<std::string>(123);
+    str = to<std::string>(123);
     EXPECT_STREQ(str.c_str(), "123");
 
-    str = util::to<std::string>(1.23);
+    str = to<std::string>(1.23);
     EXPECT_STREQ(str.c_str(), "1.23");
 
     int i;
-    i = util::to<int>("123");
+    i = to<int>("123");
     EXPECT_EQ(i, 123);
-    EXPECT_THROW(util::to<int>(1.23), util::Exception);
+    EXPECT_THROW(to<int>(1.23), Exception);
 
     double d;
-    d = util::to<double>("123");
+    d = to<double>("123");
     EXPECT_EQ(d, 123);
 
-    d = util::to<double>(123);
+    d = to<double>(123);
     EXPECT_EQ(d, 123);
+}
+
+TEST(string_helper, trim)
+{
+    std::string str = "    ";
+    EXPECT_STREQ(ltrim(str).c_str(), "");
+    EXPECT_STREQ(rtrim(str).c_str(), "");
+    EXPECT_STREQ(trim(str).c_str(), "");
+
+    str = "  abcd  ";
+    EXPECT_STREQ(ltrim(str).c_str(), "abcd  ");
+    str = "  abcd  ";
+    EXPECT_STREQ(rtrim(str).c_str(), "  abcd");
+    str = "  abcd  ";
+    EXPECT_STREQ(trim(str).c_str(), "abcd");
+
+    str = "";
+    EXPECT_STREQ(ltrim(str).c_str(), "");
+    str = "";
+    EXPECT_STREQ(rtrim(str).c_str(), "");
+    str = "";
+    EXPECT_STREQ(trim(str).c_str(), "");
 }
